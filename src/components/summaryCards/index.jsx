bfe20093ed740e2a3fcc752d24eebfc4dbefa124
Nathsanfer <158168917+Nathsanfer@users.noreply.github.com>
2025-03-11 11:56:08 -0300

    feat: create cards component

--- src/components/summaryCards/index.jsx
+++ src/components/summaryCards/index.jsx
@@ -1,37 +1,44 @@
 import styles from './summaryCards.module.css';
 
-const SummaryCards = () => {
-    return (
-        <div className={styles.summaryCards}>
-        <div className={styles.card}>
-          <h3>Arrecadação Diária</h3>
-          <div className={styles.cardValue}>R$ 12.450,00</div>
-          <div className={styles.cardChange + " " + styles.positive}>+15% em relação a ontem</div>
-        </div>
+const SummaryCards = ({ cards }) => {
+  return (
+    <div className={styles.summaryCards}>
 
-        <div className={styles.card}>
-          <h3>Arrecadação Mensal</h3>
-          <div className={styles.cardValue}>R$ 345.780,00</div>
-          <div className={styles.cardChange + " " + styles.positive}>+8% em relação ao mês passado</div>
-        </div>
-
-        <div className={styles.card}>
-          <h3>Arrecadação Semestral</h3>
-          <div className={styles.cardValue}>R$ 1.987.650,00</div>
-          <div className={styles.cardChange + " " + styles.positive}>
-            +12% em relação ao semestre anterior
+      {
+        cards.map((card, index) => (
+          <div className={styles.card}>
+            <h3>{card.title}</h3>
+            <div className={styles.cardValue}>{card.value}</div>
+            <div className={styles + `.cardChange` ${card.isPositive ? styles.positive}}>{card.change}</div>
           </div>
-        </div>
+        ))
+      }
 
-        <div className={styles.card}>
-          <h3>Ocupação Média</h3>
-          <div className={styles.cardValue}>78%</div>
-          <div className={styles.cardChange + " " + styles.negative}>
-            -3% em relação à semana anterior
-          </div>
+
+
+      <div className={styles.card}>
+        <h3>Arrecadação Mensal</h3>
+        <div className={styles.cardValue}>R$ 345.780,00</div>
+        <div className={styles.cardChange + " " + styles.positive}>+8% em relação ao mês passado</div>
+      </div>
+
+      <div className={styles.card}>
+        <h3>Arrecadação Semestral</h3>
+        <div className={styles.cardValue}>R$ 1.987.650,00</div>
+        <div className={styles.cardChange + " " + styles.positive}>
+          +12% em relação ao semestre anterior
         </div>
       </div>
-    );
+
+      <div className={styles.card}>
+        <h3>Ocupação Média</h3>
+        <div className={styles.cardValue}>78%</div>
+        <div className={styles.cardChange + " " + styles.negative}>
+          -3% em relação à semana anterior
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SummaryCards;
