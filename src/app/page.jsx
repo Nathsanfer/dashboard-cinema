545c1271d49e9751efbb69dcd1ad8897270ec8b3
Nathsanfer <158168917+Nathsanfer@users.noreply.github.com>
2025-03-11 09:33:46 -0300

    feat: create sidebar component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,21 @@
 import Header from "@/components/header";
 import styles from "./page.module.css";
 import Footer from "@/components/footer";
+import Menu from "@/components/menu";
 
 export default function Home() {
   return (
     <div>
       <Header/>
 
+      <main className={styles.dashboardContainer}>
+
+        <Menu/>
+
+      </main>
+
       <Footer/>
     </div>
   );
 }
+
