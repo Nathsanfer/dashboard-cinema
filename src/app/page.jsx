f0647df4fe4d069f3676b82783862dceb13e31fe
Nathsanfer <158168917+Nathsanfer@users.noreply.github.com>
2025-03-11 10:26:10 -0300

    feat: create dashboardContent component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@ import Header from "@/components/header";
 import styles from "./page.module.css";
 import Footer from "@/components/footer";
 import Menu from "@/components/menu";
+import DashboardContent from "@/components/dashboardContent/indexx";
 
 export default function Home() {
   return (
@@ -11,6 +12,8 @@ export default function Home() {
       <main className={styles.dashboardContainer}>
 
         <Menu/>
+
+        <DashboardContent/>
 
       </main>
 
